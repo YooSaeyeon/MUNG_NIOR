8320c00da2b8cad38221a815351d00a75e3804ee
YooSaeyeon <20201703@dongduk.ac.kr>
2023-08-07 17:37:30 +0900

    kakao login

---
+++ BE/BE/posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+# from accounts.models import User
+
+# Create your models here.
+# 질문자 model
+class Question(models.Model):
+    content = models.TextField()
+    created_at = models.DateField(verbose_name="작성일", auto_now_add=True)
+    # writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.content
+    
+# 답변자 model
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateField(verbose_name="작성일", auto_now_add=True)
+    photo = models.ImageField(verbose_name="이미지", blank=True, null=True)
+    # writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.comment
---
+++ BE/BE/posts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Question, Answer
+from .serializers import QuestionSerializer, AnswerSerializer
+from rest_framework.viewsets import ModelViewSet
+
+
+class QuestionViewSet(ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(writer = self.request.user)
+
+
+# 26번줄 헷갈리는 코드
+class AnswerViewSet(ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(writer = self.request.user)
+
+    def get_queryset(self, **kwargs): # Override
+        id = self.kwargs['question_id']
+        return self.queryset.filter(question=id)

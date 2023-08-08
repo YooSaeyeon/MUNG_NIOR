f1cc4d4c35ddc58b4a415197585ef295cfb6a063
YooSaeyeon <20201703@dongduk.ac.kr>
2023-08-08 23:39:31 +0900

    mypage update

--- BE/BE/posts/urls.py
+++ BE/BE/posts/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import SimpleRouter
 from django.urls import path, include
 from .views import QuestionViewSet, AnswerViewSet
 from rest_framework.routers import DefaultRouter
+from .views import MyQuestionsListView, MyAnswersListView
 
 questions_router = DefaultRouter()
 questions_router.register("questions", QuestionViewSet, basename="questions")
@@ -12,4 +13,6 @@ answer_router.register("answers", AnswerViewSet, basename="answer")
 urlpatterns = [
     path("", include(questions_router.urls)),
     path("questions/<int:question_id>/", include(answer_router.urls)),
+    path("my_questions/", MyQuestionsListView.as_view(), name="my-questions"),
+    path("my_answers/", MyAnswersListView.as_view(), name="my-answers"),
 ]
--- BE/BE/posts/views.py
+++ BE/BE/posts/views.py
@@ -5,7 +5,10 @@ from .models import Question, Answer
 from .serializers import QuestionSerializer, AnswerSerializer
 from rest_framework.viewsets import ModelViewSet
 
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
+# 질문자
 class QuestionViewSet(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
@@ -14,7 +17,7 @@ class QuestionViewSet(ModelViewSet):
         serializer.save(writer = self.request.user)
 
 
-# 26번줄 헷갈리는 코드
+# 답변자
 class AnswerViewSet(ModelViewSet):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
@@ -25,3 +28,24 @@ class AnswerViewSet(ModelViewSet):
     def get_queryset(self, **kwargs): # Override
         id = self.kwargs['question_id']
         return self.queryset.filter(question=id)
+    
+
+
+# 질문자 마이페이지
+class MyQuestionsListView(generics.ListAPIView):
+    serializer_class = QuestionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Question.objects.filter(writer=user)
+    
+
+# 답변자 마이페이지
+class MyAnswersListView(generics.ListAPIView):
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Answer.objects.filter(writer=user)

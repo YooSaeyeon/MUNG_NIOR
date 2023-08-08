ecef369b13b3d772b3a1d324102a0a61f451561b
woorimj <128919405+woorimj@users.noreply.github.com>
2023-08-09 02:14:28 +0900

    충돌 해결중

--- BE/BE/accounts/views.py
+++ BE/BE/accounts/views.py
@@ -109,6 +109,7 @@ class LogoutView(APIView):
 
 # code 요청
 def kakao_login(request):
+<<<<<<< HEAD
     app_rest_api_key = "942c23a0e6d71ed610295448ee869edc"
     redirect_uri = "http://127.0.0.1:8000/account/login/kakao/callback"
     return redirect(
@@ -120,3 +121,22 @@ def kakao_login(request):
 def kakao_callback(request):
     params = urllib.parse.urlencode(request.GET)
     return redirect(f"http://127.0.0.1:8000/account/login/kakao/callback?{params}")
+=======
+    app_rest_api_key = '942c23a0e6d71ed610295448ee869edc'
+    redirect_uri = "http://127.0.0.1:8000/kakao/callback"
+    redirect_url = "https://kauth.kakao.com/oauth/authorize?client_id=" + app_rest_api_key + "&redirect_uri=" + redirect_uri + "&response_type=code"
+    return redirect(redirect_url)
+    # return redirect(
+    #     f"https://kauth.kakao.com/oauth/authorize?client_id={app_rest_api_key}&redirect_uri={redirect_uri}&response_type=code"
+    # )
+
+
+# access token 요청
+def kakao_callback(request):                                                                  
+    params = urllib.parse.urlencode(request.GET)
+    redirect_url = 'http://127.0.0.1:8000/kakao/callback?' + params
+    main_home_url = "http://127.0.0.1:8000"
+    return redirect(main_home_url)
+    # return redirect(redirect_url)                                
+    # return redirect(f'http://127.0.0.1:8000/kakao/callback?{params}')
+>>>>>>> back
